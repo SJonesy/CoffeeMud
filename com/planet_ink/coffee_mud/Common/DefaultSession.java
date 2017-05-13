3b42860f4c57537c077a8c065db6b082058bf081
Stephen Jones <s_jones@wargaming.net>
2017-05-13 15:43:21 +0100

    remove line breaks on prompt

--- com/planet_ink/coffee_mud/Common/DefaultSession.java
+++ com/planet_ink/coffee_mud/Common/DefaultSession.java
@@ -2396,8 +2396,7 @@ public class DefaultSession implements Session
 				.append("'^>^<!ENTITY Move '").append(mob.curState().getMovement())
 				.append("'^>^<!ENTITY MaxMove '").append(mob.maxState().getMovement())
 				.append("'^>^<!ENTITY Exp '").append(mob.getExperience())
-				.append("'^>^<!ENTITY ExpNeed '").append(mob.getExpNeededLevel())
-				.append("'^>\n\r\n\r");
+				.append("'^>^<!ENTITY ExpNeed '").append(mob.getExpNeededLevel());
 		}
 		buf.append(CMLib.utensils().builtPrompt(mob));
 		promptPrint("^<Prompt^>"+buf.toString()+"^</Prompt^>^.^N");
